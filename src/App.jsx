a6454b1a08a4608519c3d71fb95e2ac985de5ed3
dauda femi <>
2022-09-22 14:49:10 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </div>
+  )
+}
